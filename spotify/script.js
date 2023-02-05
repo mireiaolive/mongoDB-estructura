c70b3846ea116cf65275a814cbfee5eb0cea8b35
Mireia Olivé <mirisfu@gmail.com>
2023-02-05 18:31:46 +0100

    new code

--- spotify/script.js
+++ spotify/script.js
@@ -1,1 +1,11 @@
-db.client.insertOne;
+//use spotify
+
+db.usuari.insertOne({
+    tipo: ["free", "premium"],
+    email: "jorge@gmail.com",
+    nom: "jor_81",
+    naixament: new Date(12 / 03 / 1981),
+    sexe: ["hombre", "mujer"],
+    pais: "Grecia",
+    codigo_postal: "09887",
+});
